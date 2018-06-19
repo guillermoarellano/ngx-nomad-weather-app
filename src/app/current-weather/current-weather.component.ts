89ea04e221a81299e4db22f2ac62f08981d2ee8a
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-06-19 15:25:28 -0500

    Inlined template and styles. Updated the favicon with my beauty. :)

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current-weather',
-  templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  template: `
+    <p>
+      current-weather works!
+    </p>
+  `,
+  styles: ['']
 })
 export class CurrentWeatherComponent implements OnInit {
 
