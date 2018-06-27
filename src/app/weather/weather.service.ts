1235771fb31e20394c66c95d790124d1feee7528
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-06-27 14:52:02 -0500

    Implemented getCurrentWeather method for weather service

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 interface ICurrentWeatherData {
   weather: [{
@@ -20,6 +21,12 @@ interface ICurrentWeatherData {
   providedIn: 'root'
 })
 export class WeatherService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
+  getCurrentWeather(city: string, country: string) {
+    return this.httpClient.get<ICurrentWeatherData>(
+      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?` +
+        `q=${city},${country}&appid=${environment.appId}`
+    );
+  }
 }
