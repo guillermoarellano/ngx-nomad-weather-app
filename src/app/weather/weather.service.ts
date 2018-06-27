db41929ca4d307472bb27965bedeb82119112b4f
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-06-27 14:35:40 -0500

    Imported the HttpClientModule to main App.module and defined the interface for the OpenWeather API response in WeatherService.

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface ICurrentWeatherData {
+  weather: [{
+    description: string;
+    icon: string;
+  }];
+  main: {
+    temp: number;
+  };
+  sys: {
+    country: string;
+  };
+  dt: number;
+  name: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 }
