48a6516d59fccce3e3e1e82a650431caf4c81f4c
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-06-28 17:40:44 -0500

    Null guarding using initialization. Poor practice.

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -8,7 +8,14 @@ import { WeatherService } from '../weather/weather.service';
   styles: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
-  current: ICurrentWeather;
+  current: ICurrentWeather = {
+    city: '',
+    country: '',
+    date: 0,
+    image: '',
+    temperature: 0,
+    description: ''
+  };
 
   constructor(private weatherService: WeatherService) {}
 
